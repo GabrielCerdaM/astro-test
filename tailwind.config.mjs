7bf6ac4e18685124ec9c2b2d4b176b772079243f
GabrielCerdaM <gabriel.cerda.m@gmail.com>
2024-01-02 16:35:59 -0600

    first layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors:{
-			primary: "#FA3135",
-			secondary:"#Fd8c84"
-		},
 		extend: {
+			colors: {
+				primary: "#FA3135",
+				secondary: "#Fd8c84"
+			},
 		},
 	},
 	plugins: [],
